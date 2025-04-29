2b4b0c4395576a22cddf9c59556715bb98b40452
Stiven Torres <torresmendozastiven@gmail.com>
2025-04-29 15:04:15 -0500

    add(Requests/CreatePayment) se crear un form request avanado para validar la informacion del cliente y la transaccion

---
+++ app/Http/Requests/CreatePaymentRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreatePaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'type_document' => 'required|string|max:20',
+            'number_document' => 'required|string|max:50',
+            'amount' => 'required|numeric|min:0',
+            'currency' => 'required|string|max:10',
+        ];
+    }
+}
